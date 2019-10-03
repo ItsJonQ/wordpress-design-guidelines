7e0d86ea418873e2d011393e277ed32967d1df19
Jon Q <hello@jonquach.com>
2019-10-03 12:28:25 -0400

    Format: ESLint fix root .js files

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,75 +10,79 @@ module.exports = {
 			// here. That's why we use \\u002F in the regexes below.
 			{
 				selector:
-          'ImportDeclaration[source.value=/^@wordpress\\u002F.+\\u002F/]',
-				message: 'Path access on WordPress dependencies is not allowed.',
-			},
-			{
-				selector:
-          'ImportDeclaration[source.value=/^react-spring(?!\\u002Fweb.cjs)/]',
+					'ImportDeclaration[source.value=/^@wordpress\\u002F.+\\u002F/]',
 				message:
-          'The react-spring dependency must specify CommonJS bundle: react-spring/web.cjs',
+					'Path access on WordPress dependencies is not allowed.',
 			},
 			{
 				selector:
-          'CallExpression[callee.name=/^(__|_n|_nx|_x)$/]:not([arguments.0.type=/^Literal|BinaryExpression$/])',
-				message: 'Translate function arguments must be string literals.',
+					'ImportDeclaration[source.value=/^react-spring(?!\\u002Fweb.cjs)/]',
+				message:
+					'The react-spring dependency must specify CommonJS bundle: react-spring/web.cjs',
 			},
 			{
 				selector:
-          'CallExpression[callee.name=/^(_n|_nx|_x)$/]:not([arguments.1.type=/^Literal|BinaryExpression$/])',
-				message: 'Translate function arguments must be string literals.',
+					'CallExpression[callee.name=/^(__|_n|_nx|_x)$/]:not([arguments.0.type=/^Literal|BinaryExpression$/])',
+				message:
+					'Translate function arguments must be string literals.',
 			},
 			{
 				selector:
-          'CallExpression[callee.name=_nx]:not([arguments.3.type=/^Literal|BinaryExpression$/])',
-				message: 'Translate function arguments must be string literals.',
+					'CallExpression[callee.name=/^(_n|_nx|_x)$/]:not([arguments.1.type=/^Literal|BinaryExpression$/])',
+				message:
+					'Translate function arguments must be string literals.',
 			},
 			{
 				selector:
-          'CallExpression[callee.name=/^(__|_x|_n|_nx)$/] Literal[value=/\\.{3}/]',
+					'CallExpression[callee.name=_nx]:not([arguments.3.type=/^Literal|BinaryExpression$/])',
+				message:
+					'Translate function arguments must be string literals.',
+			},
+			{
+				selector:
+					'CallExpression[callee.name=/^(__|_x|_n|_nx)$/] Literal[value=/\\.{3}/]',
 				message: 'Use ellipsis character (…) in place of three dots',
 			},
 			{
 				selector:
-          'ImportDeclaration[source.value="redux"] Identifier.imported[name="combineReducers"]',
+					'ImportDeclaration[source.value="redux"] Identifier.imported[name="combineReducers"]',
 				message: 'Use `combineReducers` from `@wordpress/data`',
 			},
 			{
 				selector:
-          'ImportDeclaration[source.value="lodash"] Identifier.imported[name="memoize"]',
+					'ImportDeclaration[source.value="lodash"] Identifier.imported[name="memoize"]',
 				message: 'Use memize instead of Lodash’s memoize',
 			},
 			{
 				selector:
-          'CallExpression[callee.object.name="page"][callee.property.name="waitFor"]',
+					'CallExpression[callee.object.name="page"][callee.property.name="waitFor"]',
 				message: 'Prefer page.waitForSelector instead.',
 			},
 			{
 				selector: 'JSXAttribute[name.name="id"][value.type="Literal"]',
 				message:
-          'Do not use string literals for IDs; use withInstanceId instead.',
+					'Do not use string literals for IDs; use withInstanceId instead.',
 			},
 			{
 				// Discourage the usage of `Math.random()` as it's a code smell
 				// for UUID generation, for which we already have a higher-order
 				// component: `withInstanceId`.
 				selector:
-          'CallExpression[callee.object.name="Math"][callee.property.name="random"]',
+					'CallExpression[callee.object.name="Math"][callee.property.name="random"]',
 				message:
-          'Do not use Math.random() to generate unique IDs; use withInstanceId instead. (If you’re not generating unique IDs: ignore this message.)',
+					'Do not use Math.random() to generate unique IDs; use withInstanceId instead. (If you’re not generating unique IDs: ignore this message.)',
 			},
 			{
 				selector:
-          'CallExpression[callee.name="withDispatch"] > :function > BlockStatement > :not(VariableDeclaration,ReturnStatement)',
+					'CallExpression[callee.name="withDispatch"] > :function > BlockStatement > :not(VariableDeclaration,ReturnStatement)',
 				message:
-          'withDispatch must return an object with consistent keys. Avoid performing logic in `mapDispatchToProps`.',
+					'withDispatch must return an object with consistent keys. Avoid performing logic in `mapDispatchToProps`.',
 			},
 			{
 				selector:
-          'LogicalExpression[operator="&&"][left.property.name="length"][right.type="JSXElement"]',
+					'LogicalExpression[operator="&&"][left.property.name="length"][right.type="JSXElement"]',
 				message:
-          'Avoid truthy checks on length property rendering, as zero length is rendered verbatim.',
+					'Avoid truthy checks on length property rendering, as zero length is rendered verbatim.',
 			},
 		],
 		'react/forbid-elements': [
